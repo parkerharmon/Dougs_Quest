57c2ec44d48119180a3495b56a76525a32801f67
Parker <parkerharmon194@gmail.com>
2023-12-07 22:09:50 -0500

    added basic doug movement

--- player/doug_character.gd
+++ player/doug_character.gd
@@ -8,10 +8,14 @@ func _physics_process(_delta):
 	#get input direction
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
-	)
+		Input.get_action_strength("down") - Input.get_action_strength("up"))
 	#update velocity
 	velocity = input_direction * move_speed
-	
+	if velocity == Vector2.ZERO:
+		$AnimationTree.get("parameters/playback").travel("Idle")
+	else:
+		$AnimationTree.get("parameters/playback").travel("Walk")
+		$AnimationTree.set("parameters/Idle/blend_position", velocity)
+		$AnimationTree.set("parameters/Walk/blend_position", velocity)
 	move_and_slide()
 
